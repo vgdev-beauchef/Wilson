268a0fc9c8f45ecf289002ddf15b5c124e02d5c3
TheAwesomeTool <pablore@me.com>
2015-07-13 23:30:49 -0300

    fix

---
+++ engine/mapGen.py
@@ -0,0 +1,13 @@
+import random
+def mapGenerator(width, height):
+	grid = []
+	for i in range(width):
+		fila = []
+		for j in range(height):
+			n = random.randint(1,100)
+			if n <= 30:
+				fila.append('#')
+			else:
+				fila.append('.')
+		grid.append(fila)
+	return grid
